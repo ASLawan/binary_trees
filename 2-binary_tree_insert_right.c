fa29d1ea33a662078cd0c0ddc79cee4fa9479ec7
ASLawan <nawalnitsua@gmail.com>
2024-02-02 09:29:09 +0100

    Fix code output error

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,9 +8,16 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newnode = binary_tree_node(parent, value);
+	binary_tree_t *newnode;
 
-	if (newnode == NULL || parent == NULL)
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	newnode = binary_tree_node(parent, value);
+
+	if (newnode == NULL)
 	{
 		return (NULL);
 	}
