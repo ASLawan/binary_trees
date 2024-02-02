3242c9f712433352cc56cc71d4e0ecfcb1c41f80
ASLawan <nawalnitsua@gmail.com>
2024-02-02 09:50:51 +0100

    Updatea: write a function to compute height if binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,25 @@
 #include "binary_trees.h"
 /**
+ * get_height - computes tree height
+ * @node: pointer to tree node
+ * @height: height of tree
+ * Return: height
+ */
+
+int get_height(const binary_tree_t *node, size_t height)
+{
+	if (node->left)
+	{
+		return (get_height(node->left, height + 1));
+	}
+	if (node->right)
+	{
+		return (get_height(node->right, height + 1));
+	}
+	return (height);
+}
+
+/**
  * binary_tree_height - computes height of binary tree
  * @tree: pointer to root node
  * Return: height or NULL
@@ -7,14 +27,19 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t lh, rh;
+	size_t lh = 0, rh = 0;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	lh = binary_tree_height(tree->left);
-	rh = binary_tree_height(tree->right);
-
-	return ((lh > rh) ? (lh + 1) : (rh + 1));
+	if (tree->left)
+	{
+		lh = get_height(tree, 0);
+	}
+	if (tree->right)
+	{
+		rh = get_height(tree, 0);
+	}
+	return (lh > rh ? lh : rh);
 }
