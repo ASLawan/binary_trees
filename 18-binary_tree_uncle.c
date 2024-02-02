af7e1c9ee7df5586fc96548a145e9b44062db74d
ASLawan <nawalnitsua@gmail.com>
2024-02-02 10:52:48 +0100

    Upate: handle node == NULL

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,11 +7,15 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	if (node == NULL)
+	{
+		return (NULL);
+	}
 	if (!node->parent)
 	{
 		return (NULL);
 	}
-	if (node == NULL || node->parent->parent == NULL)
+	if (node->parent->parent == NULL)
 	{
 		return (NULL);
 	}
